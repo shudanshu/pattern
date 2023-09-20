2677e3494fdb301d14ead0aa9a7b05a612467965
sudhanshu kumar <145319097+shudanshu@users.noreply.github.com>
2023-09-20 18:37:31 +0530

    Add files via upload

---
+++ pattern3.java
@@ -0,0 +1,10 @@
+public class pattern3 {
+    public static void main(String[] args) {
+        for(int line = 0; line<=4; line++){
+            for(int star = 0; star<=line;star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
